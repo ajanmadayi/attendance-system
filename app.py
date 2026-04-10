05f512d10441541fdfdb191e272c8e7f257f6746
ajanmadayi <57247897+ajanmadayi@users.noreply.github.com>
2026-04-10 12:25:45 +0530

    Modify Flask app to use environment variable for port
    Updated the Flask app to run on a specified port from environment variables.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Response
 import subprocess
+import os
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ def home():
 def run_script():
     def generate():
         process = subprocess.Popen(
-            ["python", "-u", "app113.py"],  # 🔥 KEY FIX
+            ["python", "-u", "app113.py"],
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             text=True,
@@ -24,5 +25,7 @@ def run_script():
 
     return Response(generate(), mimetype="text/event-stream")
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
