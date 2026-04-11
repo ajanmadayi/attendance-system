e0a4186cf4d6d52400118c008eb17c97eacfdbe2
Ajan Madayi <ajanmadayi@gmail.com>
2026-04-11 16:03:58 +0530

    rename site to main

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,4 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
-
-CMD ["gunicorn", "site:app", "--bind", "0.0.0.0:10000"]
+CMD ["gunicorn", "main:app", "--bind", "0.0.0.0:10000"]
---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h2 style='text-align:center;margin-top:100px;'>Open Attendance System</h2>
+    <div style='text-align:center;'>
+        <a href="http://203.92.32.167:8083/iclock/" target="_blank"
+           style="padding:15px 25px;background:green;color:white;
+                  text-decoration:none;font-size:18px;border-radius:5px;">
+            Open System
+        </a>
+    </div>
+    """
